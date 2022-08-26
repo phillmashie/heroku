dd7899670947ccc62f9c4f46f5360581bd875f09
Phillip Mashingaidze <phillmashie@gmail.com>
2022-08-26 10:24:15 +0200

    SERVER

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 // add routes
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to PAV contacts." });
+    res.json({ message: "Welcome to myKasa API." });
   });
 const router = require('./src/routes/router.js');
 app.use('/api', router);
